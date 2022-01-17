9a2eaf11e927c06d10ffd28d70bc83ee67a9d150
Suhayl Ahmed <suhayl.ahmed@icloud.com>
2022-01-17 22:47:30 +0000

    Added `UILabel` to display message when there are no lists yet added.

--- Shuffler/Shuffler/ListsViewController.swift
+++ Shuffler/Shuffler/ListsViewController.swift
@@ -9,11 +9,14 @@ import UIKit
 
 final class ListsViewController: UIViewController {
     
+    private let addAListLabel = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .systemGray6
         addNavigationBarButtonToAddList()
+        configureAddAListLabel()
     }
     
     private func addNavigationBarButtonToAddList() {
@@ -22,6 +25,24 @@ final class ListsViewController: UIViewController {
     
     @objc private func addList() {
         
+    }
+    
+    private func configureAddAListLabel() {
+        addAListLabel.text = "Tap the + button to add a list"
+        addAListLabel.translatesAutoresizingMaskIntoConstraints = false
+        addAListLabel.font = .preferredFont(forTextStyle: .title1)
+        addAListLabel.textAlignment = .center
+        addAListLabel.adjustsFontForContentSizeCategory = true
+        addAListLabel.numberOfLines = 0
+        
+        let horizontalSpacing: CGFloat = 24
+        
+        view.addSubview(addAListLabel)
+        NSLayoutConstraint.activate([
+            addAListLabel.leadingAnchor.constraint(equalTo: view.readableContentGuide.leadingAnchor, constant: horizontalSpacing),
+            addAListLabel.trailingAnchor.constraint(equalTo: view.readableContentGuide.trailingAnchor, constant: -horizontalSpacing),
+            addAListLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
     }
     
 }
