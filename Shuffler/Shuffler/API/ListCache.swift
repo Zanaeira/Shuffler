c1b50c20ad5cb6155cfa90bbc50f5cce170f5e92
Suhayl Ahmed <suhayl.ahmed@icloud.com>
2022-01-18 23:30:51 +0000

    Added a `ListCache` type to which the sample Lists data has been extracted

---
+++ Shuffler/Shuffler/API/ListCache.swift
@@ -0,0 +1,58 @@
+//
+//  ListCache.swift
+//  Shuffler
+//
+//  Created by Suhayl Ahmed on 18/01/2022.
+//
+
+import Foundation
+
+final class ListCache {
+    
+    private var lists: [List] = []
+    
+    func retrieve(completion: @escaping (Result<[List], Error>) -> Void) {
+        loadSampleLists()
+        
+        completion(.success(lists))
+    }
+    
+    private func loadSampleLists() {
+        lists.append(.init(name: "Manārah Y2", items: [
+            .init(text: "Abdullah"),
+            .init(text: "Ali"),
+            .init(text: "Asma"),
+            .init(text: "Aysha"),
+            .init(text: "Ferdoushi"),
+            .init(text: "Mahbub"),
+            .init(text: "Muktadeer"),
+            .init(text: "Parvez"),
+            .init(text: "Rosna"),
+            .init(text: "Dr. Khaled"),
+            .init(text: "Tahir"),
+            .init(text: "Yassin"),
+            .init(text: "Zainab"),
+            .init(text: "Hasanayn")
+        ]))
+        
+        lists.append(.init(name: "Madkhal Y1", items: [
+            .init(text: "Aysma"),
+            .init(text: "Eiram"),
+            .init(text: "Hamida"),
+            .init(text: "Ismail"),
+            .init(text: "Rabia"),
+            .init(text: "Saher"),
+            .init(text: "Saima"),
+            .init(text: "Sadiyah")
+        ]))
+        
+        lists.append(.init(name: "Hifz Class", items: [
+            .init(text: "Fatima"),
+            .init(text: "Khalid"),
+            .init(text: "Faiza"),
+            .init(text: "Erina"),
+            .init(text: "Zafir")
+        ]))
+    }
+    
+}
--- Shuffler/Shuffler/API/LocalListLoader.swift
+++ Shuffler/Shuffler/API/LocalListLoader.swift
@@ -9,50 +9,19 @@ import Foundation
 
 class LocalListLoader: ListLoader {
     
-    private var lists = [List]()
+    private var cache = ListCache()
     
     func load(completion: @escaping (Result<[List], Error>) -> Void) {
-        loadSampleLists()
-        
-        completion(.success(lists))
-    }
-    
-    private func loadSampleLists() {
-        lists.append(.init(name: "Manārah Y2", items: [
-            .init(text: "Abdullah"),
-            .init(text: "Ali"),
-            .init(text: "Asma"),
-            .init(text: "Aysha"),
-            .init(text: "Ferdoushi"),
-            .init(text: "Mahbub"),
-            .init(text: "Muktadeer"),
-            .init(text: "Parvez"),
-            .init(text: "Rosna"),
-            .init(text: "Dr. Khaled"),
-            .init(text: "Tahir"),
-            .init(text: "Yassin"),
-            .init(text: "Zainab"),
-            .init(text: "Hasanayn")
-        ]))
-        
-        lists.append(.init(name: "Madkhal Y1", items: [
-            .init(text: "Aysma"),
-            .init(text: "Eiram"),
-            .init(text: "Hamida"),
-            .init(text: "Ismail"),
-            .init(text: "Rabia"),
-            .init(text: "Saher"),
-            .init(text: "Saima"),
-            .init(text: "Sadiyah")
-        ]))
-        
-        lists.append(.init(name: "Hifz Class", items: [
-            .init(text: "Fatima"),
-            .init(text: "Khalid"),
-            .init(text: "Faiza"),
-            .init(text: "Erina"),
-            .init(text: "Zafir")
-        ]))
+        cache.retrieve { result in
+            switch result {
+            case .success(let lists):
+                completion(.success(lists))
+                return
+            case .failure(let error):
+                completion(.failure(error))
+                return
+            }
+        }
     }
     
 }
