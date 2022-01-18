cc593729cd7cc7228336d1e8cf178c6e7f33355e
Suhayl Ahmed <suhayl.ahmed@icloud.com>
2022-01-18 17:47:46 +0000

    Added `UICollectionView` to `ListItemsViewController` so that users can add Items to the list
    Changed `UIStackView` axis to horizontal to make better use of the space.

--- Shuffler/Shuffler/View Controllers/ListItemsViewController.swift
+++ Shuffler/Shuffler/View Controllers/ListItemsViewController.swift
@@ -9,6 +9,11 @@ import UIKit
 
 final class ListItemsViewController: UIViewController {
     
+    private lazy var collectionView = UICollectionView(frame: .zero, collectionViewLayout: makeCollectionViewLayout())
+    private lazy var dataSource = makeDataSource()
+    
+    private var items: [Item] = [Item(text: "Items")]
+    
     private let textField = UITextField()
     private let textFieldStackView = UIStackView()
     private let button = UIButton()
@@ -19,7 +24,31 @@ final class ListItemsViewController: UIViewController {
         
         view.backgroundColor = .systemBackground
         setupKeyboardDismissTapGestureRecognizer()
-        setupSubviews()
+        setupTextFieldAndButton()
+        configureHierarchy()
+        updateSnapshot()
+    }
+    
+    @objc private func addItem() {
+        guard let itemText = textField.text,
+              !itemText.isEmpty else { return }
+        
+        textField.resignFirstResponder()
+        textField.text = ""
+        
+        items.append(Item(text: itemText))
+        
+        updateSnapshot()
+    }
+    
+    private func updateSnapshot() {
+        guard items.count > 1 else { return }
+        
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(items, toSection: .main)
+        
+        dataSource.apply(snapshot, animatingDifferences: true)
     }
     
     private func setupKeyboardDismissTapGestureRecognizer() {
@@ -27,7 +56,7 @@ final class ListItemsViewController: UIViewController {
         view.addGestureRecognizer(tapGestureRecognizer)
     }
     
-    private func setupSubviews() {
+    private func setupTextFieldAndButton() {
         setupTextField()
         setupButton()
         setupStackView()
@@ -39,6 +68,7 @@ final class ListItemsViewController: UIViewController {
         textField.font = .preferredFont(forTextStyle: .body)
         textField.adjustsFontForContentSizeCategory = true
         textField.clearButtonMode = .whileEditing
+        textField.delegate = self
         
         textFieldStackView.translatesAutoresizingMaskIntoConstraints = false
         textFieldStackView.addArrangedSubview(textField)
@@ -53,13 +83,14 @@ final class ListItemsViewController: UIViewController {
     private func setupButton() {
         button.setTitle("Add", for: .normal)
         button.setTitleColor(.systemBlue, for: .normal)
+        button.addTarget(self, action: #selector(addItem), for: .touchUpInside)
     }
     
     private func setupStackView() {
         stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.axis = .vertical
-        stackView.spacing = 10
-        stackView.distribution = .fillProportionally
+        stackView.axis = .horizontal
+        stackView.spacing = 16
+        stackView.distribution = .fill
         [textFieldStackView, button].forEach(stackView.addArrangedSubview)
         
         view.addSubview(stackView)
@@ -68,6 +99,84 @@ final class ListItemsViewController: UIViewController {
             stackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 24),
             stackView.trailingAnchor.constraint(equalTo: view.readableContentGuide.trailingAnchor, constant: -10)
         ])
+    }
+    
+}
+
+// MARK: - UICollectionView Helpers
+extension ListItemsViewController {
+    
+    private func configureHierarchy() {
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(collectionView)
+        NSLayoutConstraint.activate([
+            collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            collectionView.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 10),
+            collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -10)
+        ])
+    }
+    
+    private func makeCollectionViewLayout() -> UICollectionViewLayout {
+        return UICollectionViewCompositionalLayout { section, layoutEnvironment in
+            var config = UICollectionLayoutListConfiguration(appearance: .insetGrouped)
+            config.headerMode = .firstItemInSection
+            
+            config.trailingSwipeActionsConfigurationProvider = { indexPath in
+                guard indexPath.item != 0 else { return UISwipeActionsConfiguration() }
+                
+                let delete = UIContextualAction(style: .destructive, title: "Delete") { [weak self] (_, _, completion) in
+                    guard let self = self else { return }
+                    
+                    self.items.remove(at: indexPath.item)
+                    self.updateSnapshot()
+                    completion(true)
+                }
+                
+                return UISwipeActionsConfiguration(actions: [delete])
+            }
+            
+            return NSCollectionLayoutSection.list(using: config, layoutEnvironment: layoutEnvironment)
+        }
+    }
+    
+    private func makeDataSource() -> UICollectionViewDiffableDataSource<Section, Item> {
+        let cellRegistration = UICollectionView.CellRegistration<UICollectionViewListCell, Item> { (cell, indexPath, item) in
+            cell.configure(with: item)
+            
+            guard indexPath.item != 0 else { return }
+            
+            let deleteAccessory: UICellAccessory = .delete(displayed: .whenEditing) { [weak self] in
+                self?.delete(item)
+            }
+            
+            cell.accessories = [deleteAccessory]
+        }
+        
+        return .init(collectionView: collectionView) { collectionView, indexPath, itemIdentifier in
+            collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: itemIdentifier)
+        }
+    }
+    
+}
+
+private extension UICollectionViewListCell {
+    
+    func configure(with item: Item) {
+        var config = defaultContentConfiguration()
+        config.text = item.text
+        
+        contentConfiguration = config
+    }
+    
+}
+
+extension ListItemsViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        addItem()
+        return true
     }
     
 }
