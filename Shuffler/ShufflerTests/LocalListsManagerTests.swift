4cdf67b5f201a5f49527d0e8e1f07513f26fe6e8
Suhayl Ahmed <suhayl.ahmed@icloud.com>
2022-01-22 23:40:09 +0000

    load sends retrieve message to cache

--- Shuffler/ShufflerTests/LocalListsManagerTests.swift
+++ Shuffler/ShufflerTests/LocalListsManagerTests.swift
@@ -10,12 +10,18 @@ import Shuffler
 
 final class LocalListsManager {
     
+    private let store: ListsStore
+    
     init(store: ListsStore) {
-        
+        self.store = store
     }
     
-    func load(completion: ([List]) -> Void) {
-        completion([])
+    func load(completion: @escaping ([List]) -> Void) {
+        store.retrieve { result in
+            if case let .success(lists) = result {
+                completion(lists)
+            }
+        }
     }
     
 }
@@ -26,7 +32,7 @@ class LocalListsManagerTests: XCTestCase {
         let listsStoreSpy = ListsStoreSpy()
         _ = LocalListsManager(store: listsStoreSpy)
         
-        XCTAssertEqual(listsStoreSpy.receivedMessages, 0)
+        XCTAssertEqual(listsStoreSpy.receivedMessages, [])
     }
     
     func test_load_onEmptyCacheReturnsEmptyLists() {
@@ -44,12 +50,24 @@ class LocalListsManagerTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
     }
     
+    func test_load_sendsRetrieveMessageToCache() {
+        let listsStoreSpy = ListsStoreSpy()
+        let sut = LocalListsManager(store: listsStoreSpy)
+        
+        sut.load() { _ in }
+        
+        XCTAssertEqual(listsStoreSpy.receivedMessages, [.retrieve])
+    }
     private class ListsStoreSpy: ListsStore {
         
-        var receivedMessages: Int = 0
+        enum Message {
+            case retrieve
+        }
+        
+        var receivedMessages: [Message] = []
         
         func retrieve(completion: @escaping (Result<[List], Error>) -> Void) {
-            
+            receivedMessages.append(.retrieve)
         }
         
         func update(_ list: List, updatedList: List, completion: @escaping (Result<[List], UpdateError>) -> Void) {
