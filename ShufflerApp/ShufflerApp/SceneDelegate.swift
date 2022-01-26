47b5b7e225f880adbf3186ba33c8be937b776159
Suhayl Ahmed <suhayl.ahmed@icloud.com>
2022-01-26 13:19:37 +0000

    Added empty project for ShufflerApp - ready for programmatic UIKit

---
+++ ShufflerApp/ShufflerApp/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  ShufflerApp
+//
+//  Created by Suhayl Ahmed on 26/01/2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+    
+}
