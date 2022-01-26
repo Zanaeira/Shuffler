cbe07e9799de41768ccc6224ece363282d47a9a1
Suhayl Ahmed <suhayl.ahmed@icloud.com>
2022-01-26 18:25:37 +0000

    Extracted protocol function for `ListsUpdater`

--- Shuffler/Shuffler/API/ListsUpdater.swift
+++ Shuffler/Shuffler/API/ListsUpdater.swift
@@ -8,6 +8,7 @@
 import Foundation
 
 public protocol ListsUpdater {
+    func insert(_ lists: [List], completion: @escaping (Result<[List],ListsStoreError>) -> Void)
     func add(_ lists: [List], completion: @escaping (Result<[List], Error>) -> Void)
     func addItem(_ item: Item, to list: List, completion: @escaping (Result<[List], ListError>) -> Void)
     func delete(_ lists: [List], completion: @escaping (Result<[List], ListError>) -> Void)
