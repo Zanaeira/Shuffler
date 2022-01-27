9c6d3a29a6aaa1a6e6bd3088bc626035541746e8
Suhayl Ahmed <suhayl.ahmed@icloud.com>
2022-01-27 00:24:29 +0000

    Composed the app in the `SceneDelegate` using a `ListNavigator` type

---
+++ ShufflerApp/ShufflerApp/ListsNavigator.swift
@@ -0,0 +1,40 @@
+//
+//  ListsNavigator.swift
+//  ShufflerApp
+//
+//  Created by Suhayl Ahmed on 26/01/2022.
+//
+
+import UIKit
+import Shuffler
+import ShuffleriOS
+
+final class ListsNavigator {
+    
+    let navigationController: UINavigationController
+    
+    private let listsManager: ListsManager
+    private let listsViewController: ListsViewController
+    
+    init(listsManager: ListsManager) {
+        self.listsManager = listsManager
+        
+        navigationController = UINavigationController()
+        let onListSelectedHandler = ListsNavigator.listSelectedHandler(listsManager: listsManager, navigationController: navigationController)
+        listsViewController = ListsViewController(listsManager: listsManager, onListSelected: onListSelectedHandler)
+        
+        navigationController.pushViewController(listsViewController, animated: true)
+    }
+    
+    private static func listSelectedHandler(listsManager: ListsManager, navigationController: UINavigationController) -> ((List) -> Void) {
+        let handler: (List) -> Void = { list in
+            let listItemsViewController = ListItemsViewController(list: list, listsManager: listsManager)
+            
+            navigationController.pushViewController(listItemsViewController, animated: true)
+        }
+        
+        return handler
+    }
+    
+}
+
--- ShufflerApp/ShufflerApp/SceneDelegate.swift
+++ ShufflerApp/ShufflerApp/SceneDelegate.swift
@@ -6,13 +6,32 @@
 //
 
 import UIKit
+import Shuffler
+import ShuffleriOS
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     
     var window: UIWindow?
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        let storeUrl = getStoreUrl()
+        let codableListsStore = CodableListsStore(storeUrl: storeUrl)
+        let localListsManager = LocalListsManager(store: codableListsStore)
+        
+        let listsNavigator = ListsNavigator(listsManager: localListsManager)
+        
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = listsNavigator.navigationController
+        window?.makeKeyAndVisible()
+    }
+    
+    private func getStoreUrl() -> URL {
+        let documentsDirectory = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask)[0]
+        
+        return documentsDirectory.appendingPathComponent("lists.store")
     }
     
 }
